8de97316fb3e79492254f87d328fb304089f9a34
Kenneth Ian Sanchez <103411799+KIPSVIII@users.noreply.github.com>
2022-08-05 19:59:18 +0800

    Update exercise2.m
    Added data type answer

--- exercise2/exercise2.m
+++ exercise2/exercise2.m
@@ -1,3 +1,3 @@
-img = imread('fruits.png'); imshow(img);
+img = imread('fruits.png'); imshow(img); #THE DATA TYPE IS UINT-8!!!
 resized = imresize(img, 0.10); imwrite(resized, 'fruits2.png'); figure(2),imshow(resized);
 hsvimg = rgb2hsv(img); imwrite(hsvimg, 'fruits3.png'); figure(3),imshow(hsvimg);
